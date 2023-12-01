290c3200964bf3f89f42978e0dbef6607628f7c8
Amaro151723 <148920071+AmaroDev@users.noreply.github.com>
2023-11-30 22:27:42 -0300

    Criado a aula004.py

---
+++ aula04.py
@@ -0,0 +1,17 @@
+#Tipos Int e Float
+#Int -> Número inteiro
+#O numero inteiro representa qualquer numero positivo. Int sem sinal é considerado positivo
+
+# Float -> Numro com ponto flutuante
+# O tipo Float representa qualquer numero positivo ou negativo com ponto flutuante.
+# Float sem sinal é considerado positivo.
+
+print(1.1, 10,11)
+print(0.0)
+
+# A função type mostra o tipo que o Pyton inferiu ao valor
+
+print(  type('Amaro')   )
+print(  type(0)   )
+print(  type(1.1)   )
+print(  type(-15.5)   )
