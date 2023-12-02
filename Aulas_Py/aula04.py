ce0490997fd9e1adebf9bccda416d6a7603d463f
Amaro151723 <148920071+AmaroDev@users.noreply.github.com>
2023-12-02 19:32:45 -0300

    Continuação das Aulas

---
+++ Aulas_Py/aula04.py
@@ -0,0 +1,51 @@
+#Tipos Int e Float
+#Int -> Número inteiro
+#O numero inteiro representa qualquer numero positivo. Int sem sinal é considerado positivo
+
+# Float -> Numro com ponto flutuante
+# O tipo Float representa qualquer numero positivo ou negativo com ponto flutuante.
+# Float sem sinal é considerado positivo.
+
+# print(1.1, 10,11)
+# print(0.0)
+
+# A função type mostra o tipo que o Pyton inferiu ao valor
+
+#-----------------------------------
+
+# print(  type('Amaro')   )
+# print(  type(0)   )
+# print(  type(1.1)   )
+# print(  type(-15.5)   )
+
+#-----------------------------------
+
+# conversão de tipos, coerção
+# type convertion,typecasting, coercion
+#é o ato de converter um tipo
+#tipos imutáveis e primitivos:
+#str, int, float, bool
+
+# print(int('1'), type(int('1')))
+# print(int('1'), type(int('1')))
+# print (bool(' '))
+# print(str(11) + 'b')
+
+#------------------------------------
+
+# Variáveis-> São usadas para salvar algo na memória do computador.
+
+# Exemplos: 
+# Full_Name = 'José Rosivaldo de Assis Amaro'
+
+#soma = 55 + 5
+
+#print (Full_Name, soma)
+
+nome = 'Amaro'
+idade = 55
+maior_de_idade = idade >= 18
+print ('Nome:', nome, 'Idade:', idade)
+
+print ('É maior?', maior_de_idade )
+
