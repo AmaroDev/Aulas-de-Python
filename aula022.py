e4fe3c49c9bb634d646060922e6eff2bbb17e6e1
Amaro151723 <148920071+AmaroDev@users.noreply.github.com>
2023-12-13 21:39:33 -0300

    aulas

---
+++ aula022.py
@@ -0,0 +1,11 @@
+numero = 20
+if numero > 1:
+    if numero > 2:
+        if numero > 3:
+            print ('Numero maior que 3')
+        else: 
+            print('Número menor que 3')
+    else: 
+        print('Número menor que 2')
+else:
+    print('Número menor que 1')
